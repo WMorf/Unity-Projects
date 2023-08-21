662531b198a86b4475461af0292919880f6de4e5
WMorf <wesmorford@outlook.com>
2023-08-21 01:41:16 -0700

    Finished Module 27

--- Castle Conquest 2D/Assets/Scripts/Player.cs
+++ Castle Conquest 2D/Assets/Scripts/Player.cs
@@ -1,25 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 15f;
 
     Rigidbody2D myRigidbody2D;
+    Animator myAnimator;
+    BoxCollider2D myBoxCollider2D;
 
     void Start()
     {
-        //Grabs RigidBody2D component
         myRigidbody2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myBoxCollider2D = GetComponent<BoxCollider2D>();
     }
 
 
     void Update()
     {
         Run();
+        Jump();
+    }
+
+    private void Jump()
+    {
+
+        if (!myBoxCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+
+        bool isJumping = CrossPlatformInputManager.GetButtonDown("Jump");
+
+        if (isJumping)
+        {
+            Vector2 jumpVelocity = new Vector2(myRigidbody2D.velocity.x, jumpSpeed);
+            myRigidbody2D.velocity = jumpVelocity;
+        }
     }
 
     private void Run()
@@ -29,8 +53,17 @@ public class Player : MonoBehaviour
 
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidbody2D.velocity.y);
         myRigidbody2D.velocity = playerVelocity;
+        //print(playerVelocity);
+
 
         FlipSprite();//Only used when running hence placement
+        ChangingToRunningState();
+    }
+
+    private void ChangingToRunningState()
+    {
+        bool runningHorizontaly = Mathf.Abs(myRigidbody2D.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", runningHorizontaly);
     }
 
     //Rotates the sprite based on it's scale
