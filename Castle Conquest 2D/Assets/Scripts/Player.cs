2c8adabc5a5d26918b8247f660fb7fa4cfc6f6b9
Wesley Morford <wesmorford@outlook.com>
2023-08-21 17:28:48 -0700

    Finished Module

--- Castle Conquest 2D/Assets/Scripts/Player.cs
+++ Castle Conquest 2D/Assets/Scripts/Player.cs
@@ -10,16 +10,23 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float runSpeed = 10f;
     [SerializeField] float jumpSpeed = 15f;
+    [SerializeField] float climbingSpeed = 8f;
 
     Rigidbody2D myRigidbody2D;
     Animator myAnimator;
     BoxCollider2D myBoxCollider2D;
+    PolygonCollider2D myPlayersFeet;
+
+    float startingGravityScale;
 
     void Start()
     {
         myRigidbody2D = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         myBoxCollider2D = GetComponent<BoxCollider2D>();
+        myPlayersFeet = GetComponent<PolygonCollider2D>();
+
+        startingGravityScale = myRigidbody2D.gravityScale;
     }
 
 
@@ -27,12 +34,34 @@ public class Player : MonoBehaviour
     {
         Run();
         Jump();
+        Climb();
+    }
+
+    private void Climb()
+    {
+        if(myBoxCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            float controlThrow = CrossPlatformInputManager.GetAxis("Vertical");
+            Vector2 climbingVelocity = new Vector2(myRigidbody2D.velocity.x, controlThrow * climbingSpeed);
+
+            myRigidbody2D.velocity = climbingVelocity;
+
+            myAnimator.SetBool("Climbing", true);
+
+            myRigidbody2D.gravityScale = 0f;
+        }
+        else
+        {
+            myAnimator.SetBool("Climbing", false);
+
+            myRigidbody2D.gravityScale = startingGravityScale;
+        }
     }
 
     private void Jump()
     {
 
-        if (!myBoxCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        if (!myPlayersFeet.IsTouchingLayers(LayerMask.GetMask("Ground")))
         {
             return;
         }
