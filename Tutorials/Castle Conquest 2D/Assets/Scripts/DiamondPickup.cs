19ff9c0e7a120369d280f9454975ee480bce85d3
Wesley Morford <wesmorford@outlook.com>
2023-09-19 16:56:45 -0700

    Complete module 44

---
+++ Tutorials/Castle Conquest 2D/Assets/Scripts/DiamondPickup.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class DiamondPickup : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
